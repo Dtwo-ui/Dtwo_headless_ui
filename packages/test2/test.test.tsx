a75b5d7c994a73f1d55cd627db30a5a9582e8165
dott <aaaa4000@naver.com>
2024-03-06 16:37:10 +0900

    example: 워크 스페이스별 테스트 동작 예제

--- packages/test2/test.test.tsx
+++ packages/test2/test.test.tsx
@@ -2,10 +2,10 @@ import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 const Com = () => {
-  return <h1>hello world2</h1>;
+  return <h1>hello world</h1>;
 };
 
-test('컴포넌트 테스트 1', () => {
+test('컴포넌트2', () => {
   render(<Com />);
   expect(screen.getByRole('heading', { name: /hello world/, level: 1 })).toBeInTheDocument();
 });
