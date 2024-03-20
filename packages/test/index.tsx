f0ad7df73fdd9c5ff49b8a4bdae10496d563f812
도비 <jm.dev.study@gmail.com>
2024-03-20 15:49:58 +0900

    test : test2 workspace dependencies 추가 및 테스트

--- packages/test/index.tsx
+++ packages/test/index.tsx
@@ -3,9 +3,9 @@ import ReactDom from 'react-dom/client';
 import { VAR1, Com1 } from './home';
 
 const root = document.querySelector('#root')!;
-export const Test = () => (
+export const Test = ({ text }: { text: string }) => (
   <div>
-    헬로 {VAR1}
+    {text} {VAR1}
     <Com1 />
   </div>
 );
--- packages/test2/index.tsx
+++ packages/test2/index.tsx
@@ -1,1 +1,8 @@
-export const Test = () => <div>헬로</div>;
+import { Test } from 'test';
+
+export const Test1 = () => (
+  <div>
+    헬로
+    <Test text="text" />
+  </div>
+);
