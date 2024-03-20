0cdc103c9a9e46900264e97f9e6b06b27e0e3478
dott <aaaa4000@naver.com>
2024-03-20 15:23:50 +0900

    config: d.ts파일 경로문제 해결 -> rollup.config.js 절대 경로 관련 수정(현재 실행 파일 경로가 아닌 스크립트 실행경로로 변경)

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,14 @@ import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import rollupPluginPeerDepsExternalModule from 'rollup-plugin-peer-deps-external';
-
+import process from 'process';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import { dts } from 'rollup-plugin-dts';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const tsconfigPath = path.resolve(__dirname, 'tsconfig.json');
-
+const tsconfigPath = path.resolve(process.cwd(), 'tsconfig.json');
 const extensions = ['.ts', '.tsx', '.js', '.jsx'];
 // const tsconfigOverride = { compilerOptions: { module: 'esnext', moduleResolution: 'node' } };
+
 export default [
   {
     input: './index.tsx',
