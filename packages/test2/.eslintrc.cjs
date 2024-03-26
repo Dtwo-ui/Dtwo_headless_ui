5dcb02b5ed096da8bb4f75ba32409090236eaeef
dott <aaaa4000@naver.com>
2024-03-26 14:38:07 +0900

    config: ts, eslint 워크스페이스 최종 설정

--- packages/test2/.eslintrc.cjs
+++ packages/test2/.eslintrc.cjs
@@ -1,15 +1,7 @@
-const path = require('path');
-const rootDir = __dirname;
-
-const tsconfigPath = path.resolve(rootDir, 'tsconfig.json');
-const base = require('../../.eslintrc.cjs');
-
 module.exports = {
-  ...base,
   root: true,
-
+  extends: ['@repo/eslint-config/ui-config.cjs'],
   parserOptions: {
-    ...base.parserOptions,
-    project: tsconfigPath,
+    project: true,
   },
 };
