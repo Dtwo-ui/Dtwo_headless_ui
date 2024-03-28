3b89280943548ea111708fba50ad5aaee6c5c5fe
dott <aaaa4000@naver.com>
2024-03-28 10:19:45 +0900

    chore: eslint project 옵션 명시적 경로 -> 실행중인 디렉토리로 변경

--- packages/test/.eslintrc.cjs
+++ packages/test/.eslintrc.cjs
@@ -1,12 +1,10 @@
 const path = require('path');
-const rootDir = __dirname;
-
-const tsconfigPath = path.resolve(rootDir, 'tsconfig.json');
 
 module.exports = {
   root: true,
   extends: ['@repo/eslint-config/ui-config.cjs'],
   parserOptions: {
-    project: tsconfigPath,
+    project: true,
+    tsconfigRootDir: __dirname,
   },
 };
