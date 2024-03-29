6a68b6f8fbbf6f8ce159fba073670d801b989c44
dott <aaaa4000@naver.com>
2024-03-29 15:49:37 +0900

    feat: Switch 컴포넌트 init

---
+++ packages/ui/switch/Switch.stories.tsx
@@ -0,0 +1,11 @@
+import { Switch } from './index';
+export default {
+  title: 'Design System/Avatar',
+  component: Switch,
+  /*
+   * More on Storybook parameters at:
+   * https://storybook.js.org/docs/react/writing-stories/parameters#component-parameters
+   */
+};
+
+export const Story = () => <Switch />;
--- packages/ui/switch/index.tsx
+++ packages/ui/switch/index.tsx
@@ -1,3 +1,36 @@
-export const Switch = () => {
-  return <div />;
+import { createContext, ReactNode, useState } from 'react';
+
+// 1. context
+
+// 2. Switch
+
+// 3. Thumb
+
+const SwitchContext = createContext({});
+
+type SwitchContextT = {
+  checked: boolean;
+  disabled: boolean;
 };
+const DEFAULT_SWITCH_CONTEXT_VALUE: SwitchContextT = { checked: false, disabled: false };
+
+const SwitchProvider = (props: object & { children: ReactNode }) => {
+  const [checked, setChecked] = useState(DEFAULT_SWITCH_CONTEXT_VALUE);
+
+  return <SwitchContext.Provider {...props} value={{ checked, setChecked }} />;
+};
+
+SwitchProvider.displayName = 'SWITCH_PROVIDER';
+
+/**
+ * TODO: forwardRef, SwitchProps 정해야함
+ * */
+export const Switch = () => {
+  return (
+    <SwitchProvider>
+      <div>테스트</div>
+    </SwitchProvider>
+  );
+};
+
+Switch.displayName = 'SWITCH';
