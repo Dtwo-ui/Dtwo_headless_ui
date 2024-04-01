4517210226b3365d836d5cc998eee90c463f71b5
도비 <jm.dev.study@gmail.com>
2024-04-01 15:35:08 +0900

    feat : Switch 컴포넌트에 context 적용

--- packages/ui/switch/index.tsx
+++ packages/ui/switch/index.tsx
@@ -1,19 +1,11 @@
-import React, {
-  createContext,
-  ReactNode,
-  useContext,
-  useState,
-  forwardRef,
-  ElementRef,
-} from 'react';
-import type { ComponentPropsWithoutRef } from '@dtwo/primitive';
+import React, { useState, forwardRef, ElementRef } from 'react';
 import { Primitive } from '@dtwo/primitive';
+import { createContext } from '@dtwo/context';
 // 1. context
 
 // 2. Switch
 
 // 3. Thumb
-const SwitchContext = createContext<SwitchContextT | object>({});
 
 type SwitchContextValueT = {
   checked: boolean;
@@ -25,29 +17,7 @@ type SwitchContextT = {
   setContextValue: React.Dispatch<SwitchContextValueT>;
 };
 
-const SwitchProvider = (
-  props: SwitchContextValueT & {
-    children: ReactNode;
-  },
-) => {
-  const { checked, disabled } = props;
-  const [contextValue, setContextValue] = useState<SwitchContextValueT>({
-    checked,
-    disabled,
-  });
-
-  return (
-    <SwitchContext.Provider
-      {...props}
-      value={{
-        contextValue,
-        setContextValue,
-      }}
-    />
-  );
-};
-
-SwitchProvider.displayName = 'SWITCH_PROVIDER';
+const [SwitchProvider, useSwitchContext] = createContext<SwitchContextT>('SWITCH');
 
 type ButtonProps = React.ComponentPropsWithoutRef<typeof Primitive.button>;
 type SwitchProps = ButtonProps & {
@@ -55,14 +25,15 @@ type SwitchProps = ButtonProps & {
   disabled?: boolean;
 };
 
-/**
- * TODO: forwardRef, SwitchProps 정해야함
- * */
 export const Switch = forwardRef<ElementRef<typeof Primitive.button>, SwitchProps>(
   ({ checked = false, disabled = false, ...props }: SwitchProps, ref) => {
     const { children } = props;
+    const [contextValue, setContextValue] = useState<SwitchContextValueT>({
+      checked,
+      disabled,
+    });
     return (
-      <SwitchProvider checked={checked} disabled={disabled}>
+      <SwitchProvider value={{ contextValue, setContextValue }}>
         <div>테스트</div>
         <Primitive.button ref={ref} {...props}></Primitive.button>
         {children}
@@ -72,9 +43,11 @@ export const Switch = forwardRef<ElementRef<typeof Primitive.button>, SwitchProp
 );
 
 export const SwitchThumb = () => {
-  const { contextValue } = useContext(SwitchContext) as SwitchContextT;
+  const {
+    contextValue: { checked, disabled },
+  } = useSwitchContext('SWITCH_THUMB');
 
-  return <input type="checkbox" checked={contextValue.checked} disabled={contextValue.disabled} />;
+  return <input type="checkbox" checked={checked} disabled={disabled} />;
 };
 
 Switch.displayName = 'SWITCH';
