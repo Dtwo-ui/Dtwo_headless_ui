d5f065d53ea45e31ac674ee3ec29bdc9b0cc97ff
도비 <jm.dev.study@gmail.com>
2024-04-04 16:20:13 +0900

    design: Switch 스토리에 컴포넌트 style 적용

--- packages/ui/switch/Switch.stories.tsx
+++ packages/ui/switch/Switch.stories.tsx
@@ -3,23 +3,36 @@ import { useState } from 'react';
 export default {
   title: 'Design System/Switch',
   component: Switch,
-  /*
-   * More on Storybook parameters at:
-   * https://storybook.js.org/docs/react/writing-stories/parameters#component-parameters
-   */
 };
 
 export const Story = () => {
   const [switchState, setSwitchState] = useState(false);
+
   return (
     <Switch
-      // checked={switchState}
-      disabled={false}
-      onChangeSwitch={() => {
-        setSwitchState(prev => !prev);
+      checked={switchState}
+      onChangeSwitch={() => setSwitchState(prev => !prev)}
+      style={{
+        display: 'flex',
+        width: '60px',
+        padding: '6px',
+        borderRadius: '9999px',
+        border: 'none',
+        justifyContent: switchState
+          ? 'end'
+          : 'start' /* control만 스타일 적용 -> unControl은 추후 처리 */,
+        background: switchState ? 'coral' : 'lightGray',
       }}
     >
-      <SwitchThumb />
+      <SwitchThumb
+        style={{
+          display: 'block',
+          width: '20px',
+          height: '20px',
+          backgroundColor: 'white',
+          borderRadius: '50%',
+        }}
+      />
     </Switch>
   );
 };
