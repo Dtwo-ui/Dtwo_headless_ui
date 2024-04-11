99642c35cd5f320c02970f7397160e0df17bb1ac
dott <aaaa4000@naver.com>
2024-04-11 15:47:45 +0900

    feat: BubbleInput proto 구현

--- packages/ui/switch/Switch.stories.tsx
+++ packages/ui/switch/Switch.stories.tsx
@@ -9,30 +9,43 @@ export const Story = () => {
   const [switchState, setSwitchState] = useState(false);
 
   return (
-    <Switch
-      checked={switchState}
-      onChangeSwitch={() => setSwitchState(prev => !prev)}
-      style={{
-        display: 'flex',
-        width: '60px',
-        padding: '6px',
-        borderRadius: '9999px',
-        border: 'none',
-        justifyContent: switchState
-          ? 'end'
-          : 'start' /* control만 스타일 적용 -> unControl은 추후 처리 */,
-        background: switchState ? 'coral' : 'lightGray',
+    <form
+      onClick={e => {
+        e.preventDefault();
+
+        console.log(e);
+        // const formData = new FormData(e.currentTarget);
+        // // console.log(formData);
+        // const data = Object.fromEntries(formData);
+        // console.log('data', data);
       }}
     >
-      <SwitchThumb
+      <Switch
+        checked={switchState}
+        name="dott"
+        onChangeSwitch={() => setSwitchState(prev => !prev)}
         style={{
-          display: 'block',
-          width: '20px',
-          height: '20px',
-          backgroundColor: 'white',
-          borderRadius: '50%',
+          display: 'flex',
+          width: '60px',
+          padding: '6px',
+          borderRadius: '9999px',
+          border: 'none',
+          justifyContent: switchState
+            ? 'end'
+            : 'start' /* control만 스타일 적용 -> unControl은 추후 처리 */,
+          background: switchState ? 'coral' : 'lightGray',
         }}
-      />
-    </Switch>
+      >
+        <SwitchThumb
+          style={{
+            display: 'block',
+            width: '20px',
+            height: '20px',
+            backgroundColor: 'white',
+            borderRadius: '50%',
+          }}
+        />
+      </Switch>
+    </form>
   );
 };
--- packages/ui/switch/index.tsx
+++ packages/ui/switch/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Primitive } from '@dtwo/primitive';
 import { createContext } from '@dtwo/context';
 import { useControllableState } from '@dtwo/use-controllable-state';
 import { useComposeRefs } from '@dtwo/use-compose-refs';
 import { composeEventHandler } from '@dtwo/utils';
 
-// 1. context
+/**
+ * Bubble Input
+ * 1. formControl 여부
+ * 2. formControl 적용시 Bubble Input 생성
+ * 3.
+ * */
+// 1. context done
 
-// 2. Switch
+// 2. Switch doing
 
-// 3. Thumb
+// 3. Thumb X
 
 type SwitchContextValueT = {
   checked?: boolean;
@@ -24,16 +30,25 @@ type SwitchProps = ButtonProps & {
   disabled?: boolean;
   defaultChecked?: boolean;
   onChangeSwitch?: (value: boolean) => void;
+  isFormControl?: boolean;
 };
 
 export const Switch = React.forwardRef<React.ElementRef<typeof Primitive.button>, SwitchProps>(
   (
-    { checked, disabled = false, defaultChecked = false, onChangeSwitch, ...props }: SwitchProps,
+    {
+      checked,
+      disabled = false,
+      defaultChecked = false,
+      onChangeSwitch,
+      isFormControl = true,
+      ...props
+    }: SwitchProps,
     forwardRef,
   ) => {
     const { children } = props;
     const buttonRef = React.useRef<HTMLButtonElement | null>(null);
     const ref = useComposeRefs(forwardRef, buttonRef);
+    const bubbleInputChange = React.useRef(false);
 
     const [switchValue, setSwitchValue] = useControllableState({
       value: checked,
@@ -45,16 +60,38 @@ export const Switch = React.forwardRef<React.ElementRef<typeof Primitive.button>
       <SwitchProvider value={{ checked: switchValue, disabled }}>
         <Primitive.button
           ref={ref}
-          onClick={composeEventHandler(props.onClick, () =>
-            setSwitchValue(prevState => !prevState),
-          )}
+          onClick={composeEventHandler(props.onClick, e => {
+            e.stopPropagation();
+            setSwitchValue(prevState => !prevState);
+            bubbleInputChange.current = !bubbleInputChange.current;
+          })}
           {...props}
-        ></Primitive.button>
+        />
+        {isFormControl && (
+          <FakeInput isBubbleChange={bubbleInputChange.current} name={props.name} />
+        )}
         {children}
       </SwitchProvider>
     );
   },
 );
+
+type FakeInputProps = React.ComponentPropsWithoutRef<'input'> & {
+  isBubbleChange: boolean;
+};
+const FakeInput = (props: FakeInputProps) => {
+  const { isBubbleChange } = props;
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const input = inputRef.current as HTMLInputElement;
+
+    input.click();
+    input.checked = isBubbleChange;
+  }, [isBubbleChange]);
+
+  return <input type="checkbox" ref={inputRef} {...props} />;
+};
 
 type SwitchThumbProps = React.ComponentPropsWithoutRef<typeof Primitive.span>;
 export const SwitchThumb = (props: SwitchThumbProps) => {
