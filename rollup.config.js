26ddd1a614c96559defda4f7ea2190c38a7903f7
dott <aaaa4000@naver.com>
2024-04-17 15:26:54 +0900

    config: rollup entry point 각 워크스페이스별 package.json의 index를 이용해서 되도록 설정

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,28 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import rollupPluginPeerDepsExternalModule from 'rollup-plugin-peer-deps-external';
 import process from 'process';
-import path from 'path';
+import path, { resolve } from 'path';
+import { readFileSync } from 'fs';
 
+function readWorkspacePackageJson() {
+  const workspacePackageJsonPath = resolve(process.cwd(), 'package.json');
+  try {
+    const workspacePackageJsonContent = readFileSync(workspacePackageJsonPath, 'utf8');
+    return JSON.parse(workspacePackageJsonContent);
+  } catch (error) {
+    console.error('Failed to read workspace package.json:', error);
+    return null;
+  }
+}
+
+// 사용 예시
+const workspacePackageJson = readWorkspacePackageJson();
 const tsconfigPath = path.resolve(process.cwd(), 'tsconfig.json');
 const extensions = ['.ts', '.tsx', '.js', '.jsx'];
 
 export default [
   {
-    input: './index.tsx',
+    input: workspacePackageJson['index'],
     output: [
       {
         dir: './dist',
