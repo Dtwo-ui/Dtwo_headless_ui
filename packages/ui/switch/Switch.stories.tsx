f88e5ce2f382815e803fdd3ad36af6843c568866
도비 <jm.dev.study@gmail.com>
2024-04-24 14:17:34 +0900

    refactor: Switch 스토리를 controlled/uncontrolled로 분리

--- packages/ui/switch/Switch.stories.tsx
+++ packages/ui/switch/Switch.stories.tsx
@@ -6,37 +6,16 @@ export default {
   component: Switch,
 };
 
-export const Story = () => {
-  // const [switchState, setSwitchState] = useState(false);
+export const FormUncontrolled = () => {
   return (
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-      }}
-      onChange={() => {
-        console.log('변경이 되야하는데');
-      }}
-      onClick={() => {
-        console.log('클릭이벤트 발생');
-      }}
-      style={{ backgroundColor: 'red' }}
-    >
+    <form onSubmit={e => e.preventDefault()}>
       <Switch.Root
-        // checked={switchState}
-        name="dott"
-        // required
-        // checked
-        // onChange={() => setSwitchState(prev => !prev)}
         style={{
           display: 'flex',
           width: '60px',
           padding: '6px',
           borderRadius: '9999px',
           border: 'none',
-          // justifyContent: switchState
-          //   ? 'end'
-          //   : 'start' /* control만 스타일 적용 -> unControl은 추후 처리 */,
-          // background: switchState ? 'coral' : 'lightGray',
         }}
       >
         <Switch.Thumb
@@ -49,7 +28,41 @@ export const Story = () => {
           }}
         />
       </Switch.Root>
-      <button type={'submit'}>서브밋</button>
+      <button type="submit">서브밋</button>
+    </form>
+  );
+};
+
+export const FormControlled = () => {
+  const [switchState, setSwitchState] = useState(false);
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <Switch.Root
+        checked={switchState}
+        onChangeSwitch={() => setSwitchState(prev => !prev)}
+        style={{
+          display: 'flex',
+          width: '60px',
+          padding: '6px',
+          borderRadius: '9999px',
+          border: 'none',
+          justifyContent: switchState
+            ? 'end'
+            : 'start' /* control만 스타일 적용 -> unControl은 추후 처리 */,
+          background: switchState ? 'coral' : 'lightGray',
+        }}
+      >
+        <Switch.Thumb
+          style={{
+            display: 'block',
+            width: '20px',
+            height: '20px',
+            backgroundColor: 'white',
+            borderRadius: '50%',
+          }}
+        />
+      </Switch.Root>
     </form>
   );
 };
