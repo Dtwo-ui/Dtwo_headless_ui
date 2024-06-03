304f8025b6330d80aabfcbf835a6582ec6c13136
dott <aaaa4000@naver.com>
2024-06-03 20:06:49 +0900

    feat: Dailog 루트 프로토 구현

--- packages/ui/dialog/Dialog.tsx
+++ packages/ui/dialog/Dialog.tsx
@@ -1,4 +1,6 @@
-// import { createContext } from '@d_two/context';
+import { createContext } from '@d_two/context';
+import { useControllableState } from '@d_two/use-controllable-state';
+import { useContext } from 'react';
 
 import { Portal } from './Portal';
 
@@ -14,6 +16,46 @@ type DialogPortalProps = React.ComponentPropsWithoutRef<typeof Portal> & {
 const DialogPortal = (props: DialogPortalProps) => {
   const { children, container } = props;
   return <Portal container={container}>{children}</Portal>;
+};
+
+/*-----*/
+
+type DialogContextValueT = {
+  modal: boolean;
+  open: boolean;
+  onOpenChange(open: boolean): void;
+};
+
+const [DialogProvider, useDialogContext] = createContext<DialogContextValueT>('DIALOG_PROVIDER');
+
+type DialogRootProps = Partial<{
+  modal: boolean;
+  open: boolean;
+  onOpenChange(open: boolean): void;
+  children?: React.ReactNode;
+}>;
+
+const DialogRoot = ({
+  modal = true,
+  open = false,
+  onOpenChange,
+  children,
+  ...props
+}: DialogRootProps) => {
+  const [controlledOpen, setControlledOpen] = useControllableState({
+    value: open,
+    defaultValue: false,
+    onChange: onOpenChange,
+  });
+
+  return (
+    <DialogProvider
+      value={{ modal, open: controlledOpen, onOpenChange: setControlledOpen }}
+      {...props}
+    >
+      {children}
+    </DialogProvider>
+  );
 };
 
 export const Dialog = { Portal: DialogPortal };
